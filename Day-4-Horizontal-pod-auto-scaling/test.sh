9dde12aebc905261d11560e96b330885795a938e
Yogeshwar Walmik Deshmukh <deshmukh.yogesh333@gmail.com>
2026-05-12 09:44:00 +0530

    Update test.sh

--- Day-4-Horizontal-pod-auto-scaling/test.sh
+++ Day-4-Horizontal-pod-auto-scaling/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a1d3e30f270ac419bb01dcb7471a08c3-1211780315.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://aa5de15d7cbcc4deab04907aad750224-1026419156.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
